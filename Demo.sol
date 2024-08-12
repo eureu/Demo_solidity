97abaa26907514d8572e231aae68afe0f8b7bdd3
Veniamin <veniamin@Air-Veniamin.lan>
2024-08-12 20:34:21 +0300

    add: demo.sol

---
+++ Demo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+contract MyContract {
+    address owner;
+
+    constructor() {
+        // code for deployment
+        owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    }
+}
+
+contract
